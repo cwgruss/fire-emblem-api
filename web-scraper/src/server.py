3e243be0ef0b69774b5568a8e6cbe0f6d5df9da6
Carter Gruss <cwgruss@gmail.com>
2020-10-20 07:58:10 -0500

    feat: Break out application into microservices
    Begin the microservice archtecture with a web-scraper app.

---
+++ web-scraper/src/server.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from crawler import Crawler, Website
+
+website = Website('Fire Emblem: Three Houses',
+                  'https://fireemblem.fandom.com/wiki/Fire_Emblem:_Three_Houses', 'h1', 'div.WikiaArticle')
+
+crawler = Crawler()
+
+
+def make_server():
+    server = Flask(__name__)
+
+    @server.route("/")
+    def hello():
+        content = crawler.parse(
+            website, 'https://fireemblem.fandom.com/wiki/Fire_Emblem:_Three_Houses')
+        res = content.toString()
+        return res
+
+    return server
+
+
+if __name__ == "__main__":
+    server = make_server()
+    server.run(host='0.0.0.0', port=8080)
